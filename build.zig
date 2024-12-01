37fc99aaa512e2cf12db24d5d79759d00fcff722
fardragon <michaldrozd@protonmail.ch>
2024-12-01 10:44:29 +0100

    Reorganize code for 2024

--- build.zig
+++ build.zig
@@ -1,16 +1,20 @@
 const std = @import("std");
 
-pub fn buildDay(b: *std.Build, target: *const std.Build.ResolvedTarget, optimize_options: *const std.builtin.Mode, all_tests_step: *std.Build.Step, comptime name: []const u8) void {
-    const path = "src/" ++ name ++ "/main.zig";
-
-    const common_module = b.createModule(.{
-        .root_source_file = b.path("src/common/common.zig"),
-        // .dependencies = &.{},
-    });
+fn buildDay(
+    b: *std.Build,
+    target: *const std.Build.ResolvedTarget,
+    optimize_options: *const std.builtin.Mode,
+    common_module: *std.Build.Module,
+    all_tests_step: *std.Build.Step,
+    comptime year: []const u8,
+    comptime name: []const u8,
+) void {
+    const path = "src/" ++ year ++ "/" ++ name;
+    const full_name = year ++ "_" ++ name;
 
     const exe = b.addExecutable(.{
-        .name = name,
-        .root_source_file = b.path(path),
+        .name = full_name,
+        .root_source_file = b.path(path ++ "/main.zig"),
         .target = target.*,
         .optimize = optimize_options.*,
     });
@@ -23,56 +27,59 @@ pub fn buildDay(b: *std.Build, target: *const std.Build.ResolvedTarget, optimize
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-    run_cmd.cwd = b.path("src/" ++ name);
+    run_cmd.cwd = b.path(path);
 
-    const run_step = b.step("run_" ++ name, "Run " ++ name);
+    const run_step = b.step("run_" ++ year ++ "_" ++ name, "Run " ++ name);
     run_step.dependOn(&run_cmd.step);
 
     const unit_tests = b.addTest(.{
-        .root_source_file = b.path(path),
+        .root_source_file = b.path(path ++ "/main.zig"),
         .target = target.*,
         .optimize = optimize_options.*,
     });
     unit_tests.root_module.addImport("common", common_module);
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
-    run_unit_tests.cwd = b.path("src/" ++ name);
+    run_unit_tests.cwd = b.path(path);
 
-    const test_step = b.step("test_" ++ name, "Run " ++ name ++ " unit tests");
+    const test_step = b.step("test_" ++ year ++ "_" ++ name, "Run " ++ name ++ " unit tests");
     test_step.dependOn(&run_unit_tests.step);
     all_tests_step.dependOn(&run_unit_tests.step);
+}
+
+fn buildYear(
+    b: *std.Build,
+    target: *const std.Build.ResolvedTarget,
+    optimize_options: *const std.builtin.Mode,
+    common_module: *std.Build.Module,
+    all_tests_step: *std.Build.Step,
+    comptime year: u16,
+    comptime days: u8,
+) void {
+    const year_buf = std.fmt.comptimePrint("{}", .{year});
+    inline for (1..(days + 1)) |day| {
+        const day_buf = std.fmt.comptimePrint("{}", .{day});
+        buildDay(
+            b,
+            target,
+            optimize_options,
+            common_module,
+            all_tests_step,
+            year_buf,
+            "day_" ++ day_buf,
+        );
+    }
 }
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const common_module = b.createModule(.{
+        .root_source_file = b.path("src/common/common.zig"),
+    });
+
     const run_all_tests_step = b.step("test_all", "Run all tests");
 
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_test");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_1");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_2");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_3");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_4");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_5");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_6");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_7");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_8");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_9");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_10");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_11");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_12");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_13");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_14");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_15");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_16");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_17");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_18");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_19");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_20");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_21");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_22");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_23");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_24");
-    buildDay(b, &target, &optimize, run_all_tests_step, "day_25");
+    buildYear(b, &target, &optimize, common_module, run_all_tests_step, 2023, 25);
 }
